e1ac9d86d845e0c2246fca1c047da8685ddaaa9c
jaustinj <austinjacobs@gmail.com>
2018-03-06 23:09:51 -0800

    UPDATE: Get more info from API

--- AddressMapper.py
+++ AddressMapper.py
@@ -22,16 +22,20 @@ class GoogleMapper(object):
     Note, standard google API allows for 2500 free requests
     per day, with a max of 50/sec queries.  Make sure not
     to go over these limits or you may be charged
+
+    Without an API KEY this script will only pull 1-3 addresses
+    Before throwing an error
     '''
 
     URL = 'https://maps.googleapis.com/maps/api/geocode/json?address={address}&key={api_key}'
     
     def __init__(self, address):
         self.name = address
+        self.geo_attributes = None
         self._main()
         
     def __repr__(self):
-        return str(self.__dict__)
+        return str(self.geo_attributes)
     
     def _encode_name(self):
         return self.name.replace(',', ''
@@ -39,42 +43,59 @@ class GoogleMapper(object):
                        ).replace(' ', '+')
     
     def _parse_result(self, result):
+        attrs = {}
+
         r_str = result.decode('utf8')
         r_json = json.loads(r_str)
+
         lat_lon = r_json['results'][0]['geometry']['location']
-        lat = lat_lon['lat']
-        lon = lat_lon['lng']
+        attrs['lat'] = lat_lon['lat']
+        attrs['lon'] = lat_lon['lng']
+
+        address_components = r_json['results'][0]['address_components']
+        attrs['city_long'] = address_components[0]['long_name']
+        attrs['city_short'] = address_components[0]['short_name']
+        attrs['county_long'] = address_components[1]['long_name']
+        attrs['county_short'] = address_components[1]['short_name']
+        attrs['state_long'] = address_components[2]['long_name']
+        attrs['state_short'] = address_components[2]['short_name']
+        attrs['country_long'] = address_components[0]['long_name']
+        attrs['country_short'] = city_long = address_components[0]['short_name']
+
+        attrs['name'] = self.name
         
-        return lat, lon
+        return attrs
     
     def _main(self):
         encoded_name = self._encode_name()
         formatted_url = self.URL.format(address=encoded_name, api_key=API_KEY)
         
         r = requests.get(formatted_url).content
-        self.lat, self.lon = self._parse_result(r)
+        self.geo_attributes = self._parse_result(r)
 
 
 if __name__ == '__main__':
-	# convert str of addresses into list
-	address_list = ADDRESSES.split('\n')
-	# In case extra spaces introduced in config file
-	address_list = [a for a in address_list if a != '']
+    # grab addresses from config file
+    address_list = ADDRESSES
 
-	# Use GoogleMapper Class to parse results from Google API
-	geo_list = []
-	for ind, name in enumerate(address_list):
-	    G = GoogleMapper(name)
-	    geo_list.append(G)
-	    print('Finished {}/{}'.format(ind+1, len(address_list)))
-	    time.sleep(0.1) #Google Standard Max = 50/sec
+    # Use GoogleMapper Class to parse results from Google API
+    geo_list = []
+    for ind, name in enumerate(address_list):
+        G = GoogleMapper(name)
+        geo_list.append(G)
+        print('Finished {}/{}'.format(ind+1, len(address_list)))
+        time.sleep(0.1) #Google Standard Max = 50/sec
 
-	#convert list of addresses to pandas df
-	d = {'name': [a.name for a in geo_list],
-	     'lat': [a.lat for a in geo_list],
-	     'lon': [a.lon for a in geo_list]
-	    }
-	df = pd.DataFrame(d)[['name', 'lat', 'lon']]
-	df.to_csv(OUTPUT_FILE, index=False)
+    #convert list of addresses to pandas df
+    d = {}
+    for key in G.geo_attributes.keys():
+        d[key] = [a.geo_attributes[key] for a in geo_list]
 
-	print('DONE \nFile written to: {}'.format(OUTPUT_FILE))
+    df = pd.DataFrame(d)[['name', 'lat', 'lon', 
+                          'city_short', 'city_long', 'county_short', 
+                          'county_long', 'state_short', 'state_long', 
+                          'country_short', 'country_long']]
+
+    df.to_csv(OUTPUT_FILE, index=False)
+
+    print('DONE \nFile written to: {}'.format(OUTPUT_FILE))
--- config.py
+++ config.py
@@ -11,99 +11,101 @@ OUTPUT_FILE = ''
 
 # String of colleges to scrape.  Every college should
 # Be put on a separate line (Separated by newline \n)
-ADDRESSES = '''Arizona State University
-Auburn University
-Ball State University
-Brandeis University
-Brigham Young University
-Brown University
-Boston University
-Carnegie Mellon University
-Colorado State University
-Columbia University
-Cornell UniversityAP
-Dalhousie University
-DePaul University
-Drexel University
-Duke University
-Georgia Institute of Technology
-Georgia State University
-Harvard University
-Indiana University
-Iowa State University
-Johns Hopkins University
-Kansas State University
-Massachusetts Institute of Technology
-Michigan State University
-Mississippi State University
-Mississippi State University Artificial Intelligence Home Page
-New Jersey Institute of Technology
-New Mexico State University
-New York University
-North Carolina State University
-Northwestern University
-Northwestern University, Qualitative Reasoning Group
-The Ohio State University
-Oregon Health & Science University
-Oregon State University
-Pace University
-Purdue University
-Rensselaer Polytechnic Institute
-Rice University
-Rutgers University
-Stanford University
-State University of New York, Buffalo
-Syracuse University
-Temple University
-Texas A&M University
-Texas Tech University
-Tufts University
-Tulane University
-University of Alabama in Huntsville
-University of Arkansas
-University of California, Berkeley
-University of California, Davis
-University of California, Irvine
-University of California, Los Angeles
-University of California, San Diego
-University of California, Santa Cruz
-University of Central Florida
-University of Chicago
-University of West Florida
-University of Georgia
-University of Illinois, Chicago
-University of Illinois, Urbana Champaign
-University of Iowa
-University of Kansas
-University of Kentucky
-University of Louisville
-University of Maine
-University of Maryland, College Park
-University of Maryland, Baltimore County
-University of Massachusetts, Amherst
-University of Michigan
-University of Minnesota
-University of New Hampshire
-University of New Mexico
-University of Oregon
-University of Pennsylvania
-University of Pittsburgh
-University of Rochester
-University of South Carolina
-University of Southern California
-University of Southern California Information Sciences Institute
-University of Southern California Institute for Creative Technologies
-University of Tennessee, Knoxville
-University of Texas, Austin
-University of Texas, Dallas
-University of Utah
-University of Vermont
-University of Virginia
-University of Washington
-University of Wisconsin, Madison
-University of Wisconsin, Milwaukee
-Washington University in St. Louis
-Wayne State University
-Worcester Polytechnic Institute
-Wright State University
-Yale University'''
+ADDRESSES = [
+    'Arizona State University',
+    'Auburn University',
+    'Ball State University',
+    'Brandeis University',
+    'Brigham Young University',
+    'Brown University',
+    'Boston University',
+    'Carnegie Mellon University',
+    'Colorado State University',
+    'Columbia University',
+    'Cornell University',
+    'Dalhousie University',
+    'DePaul University',
+    'Drexel University',
+    'Duke University',
+    'Georgia Institute of Technology',
+    'Georgia State University',
+    'Harvard University',
+    'Indiana University',
+    'Iowa State University',
+    'Johns Hopkins University',
+    'Kansas State University',
+    'Massachusetts Institute of Technology',
+    'Michigan State University',
+    'Mississippi State University',
+    'Mississippi State University Artificial Intelligence Home Page',
+    'New Jersey Institute of Technology',
+    'New Mexico State University',
+    'New York University',
+    'North Carolina State University',
+    'Northwestern University',
+    'Northwestern University, Qualitative Reasoning Group',
+    'The Ohio State University',
+    'Oregon Health & Science University',
+    'Oregon State University',
+    'Pace University',
+    'Purdue University',
+    'Rensselaer Polytechnic Institute',
+    'Rice University',
+    'Rutgers University',
+    'Stanford University',
+    'State University of New York, Buffalo',
+    'Syracuse University',
+    'Temple University',
+    'Texas A&M University',
+    'Texas Tech University',
+    'Tufts University',
+    'Tulane University',
+    'University of Alabama in Huntsville',
+    'University of Arkansas',
+    'University of California, Berkeley',
+    'University of California, Davis',
+    'University of California, Irvine',
+    'University of California, Los Angeles',
+    'University of California, San Diego',
+    'University of California, Santa Cruz',
+    'University of Central Florida',
+    'University of Chicago',
+    'University of West Florida',
+    'University of Georgia',
+    'University of Illinois, Chicago',
+    'University of Illinois, Urbana Champaign',
+    'University of Iowa',
+    'University of Kansas',
+    'University of Kentucky',
+    'University of Louisville',
+    'University of Maine',
+    'University of Maryland, College Park',
+    'University of Maryland, Baltimore County',
+    'University of Massachusetts, Amherst',
+    'University of Michigan',
+    'University of Minnesota',
+    'University of New Hampshire',
+    'University of New Mexico',
+    'University of Oregon',
+    'University of Pennsylvania',
+    'University of Pittsburgh',
+    'University of Rochester',
+    'University of South Carolina',
+    'University of Southern California',
+    'University of Southern California Information Sciences Institute',
+    'University of Southern California Institute for Creative Technologies',
+    'University of Tennessee, Knoxville',
+    'University of Texas, Austin',
+    'University of Texas, Dallas',
+    'University of Utah',
+    'University of Vermont',
+    'University of Virginia',
+    'University of Washington',
+    'University of Wisconsin, Madison',
+    'University of Wisconsin, Milwaukee',
+    'Washington University in St. Louis',
+    'Wayne State University',
+    'Worcester Polytechnic Institute',
+    'Wright State University',
+    'Yale University'
+]
